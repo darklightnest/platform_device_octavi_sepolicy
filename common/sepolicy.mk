f7a31658adf5e343b8d4bdf1b010a1a009dd755c
theimpulson <theimpulson@users.noreply.github.com>
2020-11-05 01:50:47 +0000

    sepolicy: Switch to BOARD_VENDOR_SEPOLICY_DIRS
    - BOARD_SEPOLICY_DIRS is deprecated and gives compile-time
      errors when used in unison with a device using BOARD_VENDOR_SEPOLICY_DIRS
    
    Ref:
    [0]: LineageOS/android_system_sepolicy@ec3ac47
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: Icefb062cc8cdef532b4310684d9a66afe97e49c4

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -26,7 +26,7 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/octavi/sepolicy/common/dynamic \
     device/octavi/sepolicy/common/system
 else
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
     device/octavi/sepolicy/common/dynamic \
     device/octavi/sepolicy/common/vendor
 endif
