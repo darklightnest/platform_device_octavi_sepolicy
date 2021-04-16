LineageOS/android_device_lineage_sepolicy

qcom: add msm8960 and apq8084 into upstream list
msm8960 and apq8084 also needs this upstream for its sepolicy to avoid breaking the build

Change-Id: I2dd836278cc6f899021679bdaddf339bd29141b0

qcom/sepolicy.mk
@@ -22,12 +22,12 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/lineage/sepolicy/qcom/vendor
 endif
 
-ifneq ($(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter apq8084 msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8960 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/lineage/sepolicy/qcom/legacy-vendor
 endif
 
-ifeq (,$(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
+ifeq (,$(filter apq8084 msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8960 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
 BOARD_SEPOLICY_M4DEFS += \
     qdisplay_service=vendor_qdisplay_service \
     sysfs_graphics=vendor_sysfs_graphics \

darklightnest/platform_device_octavi_sepolicy

qcom/sepolicy.mk
@@ -22,12 +22,12 @@ BOARD_VENDOR_SEPOLICY_DIRS += \
     device/octavi/sepolicy/qcom/vendor
 endif
 
-ifneq ($(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8960 msm8226 msm8610 msm8974 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994,$(TARGET_BOARD_PLATFORM)),)
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/octavi/sepolicy/qcom/legacy-vendor
 endif
 
-ifeq (,$(filter msm8226 msm8610 msm8974 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
+ifeq (,$(filter msm8960 msm8226 msm8610 msm8974 apq8084 msm8909 msm8916 msm8952 msm8992 msm8994 msm8937 msm8953 msm8996 msm8998 sdm660 sdm710 sdm845, $(TARGET_BOARD_PLATFORM)))
 BOARD_SEPOLICY_M4DEFS += \
     qdisplay_service=vendor_qdisplay_service \
     sysfs_graphics=vendor_sysfs_graphics \